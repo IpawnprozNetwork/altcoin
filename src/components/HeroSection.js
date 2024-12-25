8dee1099c3cdea8b54c3907f80ae4adc20bffcc3
Carlos Verbelen <merovingian369@gmail.com>
2024-12-25 13:52:41 -0300

    Added video background or GIF

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import backgroundImage from '../assets/background-v2.jpg'; // Make sure this path is correct
-
 import './HeroSection.css';
 
 const HeroSection = () => {
   return (
-    <div className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className="hero">
+      <video autoPlay loop muted className="background-video">
+        <source src="https://stonkscoin.org/images/stocksbg.gif" type="video/mp4" />
+        {/* You can add additional formats for compatibility */}
+        <source src="https://stonkscoin.org/images/stocksbg.gif" type="video/webm" />
+      </video>
       <h1>Welcome to Trust in Pawgress</h1>
       <p>Join us on our journey!</p>
     </div>
